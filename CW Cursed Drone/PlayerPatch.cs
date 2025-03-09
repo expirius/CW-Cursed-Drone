27b5f829efe1e2d4d2ca7b820b4648b83c5a0889
expirius <vladworldof@gmail.com>
2025-03-09 22:49:52 +0300

    tihs* not working. But fixed patch

--- CW Cursed Drone/PlayerPatch.cs
+++ CW Cursed Drone/PlayerPatch.cs
@@ -1,8 +1,11 @@
-﻿using HarmonyLib;
+﻿using DefaultNamespace.Artifacts;
+using HarmonyLib;
 using Photon.Pun;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
+using Object = UnityEngine.Object;
 
 namespace CW_Cursed_Drone
 {
@@ -10,15 +13,35 @@ namespace CW_Cursed_Drone
     [HarmonyPatch("Update")]
     public class PlayerPatch
     {
+        private GameObject droneObject;
+        private PhotonView m_PhotonView;
+        public static DroneKeySetting SpawnButton = GameHandler.Instance.SettingsHandler.GetSetting<DroneKeySetting>();
+
         [HarmonyPostfix]
         public static void SpawnDroneOnInput(Player __instance)
         {
-            // проверяем нажата ли кнопка
-            if (!GlobalInputHandler.GetKeyDown(DroneKeySetting.SummonKey))
-                return;
-            Debug.Log($"[Cursed Drone] Pressed Summon key {DroneKeySetting.SummonKey}");
-            DronePlugin drone = new();
-            //Debug.Log($"[Cursed Drone] drone name: {_currentDrone.name} spawn pos: {spawnPos}");
+            bool flag = Input.GetKeyDown(SpawnButton.Keycode()); //&& Player.localPlayer.data.dead && PhotonGameLobbyHandler.IsSurface;
+            if (flag)
+            {
+                Debug.Log($"[Cursed Drone] Pressed Summon key {SpawnButton.Keycode()}");
+
+                //Player.localPlayer.CallRevive();
+
+                this.m_PhotonView.RPC("RPCA_SpawnCursedDrone", RpcTarget.All);
+
+                Object.Instantiate<GameObject>(this.droneObject, Vector3.zero, Quaternion.Euler(new Vector3(0f, -30f, 0f))).GetComponent<CursedDrone>();
+
+                Debug.Log($"[Cursed Drone] Spawned: ");
+
+            }
+        }
+        [PunRPC]
+        private void RPCA_SpawnCursedDrone()
+        {
+            //List<Item> list = new List<Item>();
+
+            Object.Instantiate<GameObject>(this.droneObject, Vector3.zero, Quaternion.Euler(new Vector3(0f, -30f, 0f))).GetComponent<CursedDrone>();
         }
     }
+
 }
