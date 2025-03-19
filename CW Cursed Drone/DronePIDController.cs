c6dbf9f09b55edeca0d6a1def53b19573a85f21d
expirius <vladworldof@gmail.com>
2025-03-19 23:54:59 +0300

    DronePIDController: A simplified PID controller (without an integral component) for 3D vectors.

---
+++ CW Cursed Drone/DronePIDController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace CW_Cursed_Drone
+{
+    public class DronePIDController
+    {
+        private Vector3 pd;
+
+        private Vector3 p;
+        private Vector3 d;
+        private Vector3 lastE;
+
+        public DronePIDController()
+        {
+
+        }
+
+        public Vector3 CalculatePD(Vector3 e, float pk, float dk)
+        {
+            p = e * pk;
+            d = ((e - lastE) / Time.fixedDeltaTime) * dk;
+            pd = p + d;
+
+            lastE = e;
+            return pd;
+        }
+    }
+}
