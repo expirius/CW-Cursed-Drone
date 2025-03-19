d59c9e65f236824eac3fddb9a41238484074439e
expirius <vladworldof@gmail.com>
2025-03-19 11:52:11 +0300

    Added Plugin dir path and LoadAssetBundle for bundle drone

--- CW Cursed Drone/CursedDroneInfo.cs
+++ CW Cursed Drone/CursedDroneInfo.cs
@@ -1,10 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Reflection;
+using UnityEngine;
 
 namespace CW_Cursed_Drone
 {
     [ContentWarningPlugin("Cursed Drone", "1.0.0", false)]
     public class CursedDroneInfo
     {
-        static CursedDroneInfo() => Debug.Log($"[{nameof(CursedDroneInfo)}] loaded!");
+        public static AssetBundle _bundle;
+
+        static CursedDroneInfo()
+        {
+            LoadAssetBundle();
+            Debug.Log($"[{nameof(CursedDroneInfo)}] loaded!");
+        }
+        static string PluginDirPath
+        {
+            get
+            {
+                return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            }
+        }
+        private static void LoadAssetBundle()
+        {
+            CursedDroneInfo._bundle = AssetBundle.LoadFromFile(Path.Join(CursedDroneInfo.PluginDirPath, "curseddrone"));
+        }
     }
 }
