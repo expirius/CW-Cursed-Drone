e6625fdc63921c6e3d15ceae707297ebef87ae5f
expirius <vladworldof@gmail.com>
2025-03-19 23:51:26 +0300

    For future

---
+++ CW Cursed Drone/CursedDroneRPC.cs
@@ -0,0 +1,19 @@
+﻿using Photon.Pun;
+using Sirenix.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace CW_Cursed_Drone
+{
+    public class CursedDroneRPC : MonoBehaviourPunCallbacks
+    {
+        [PunRPC]
+        public void RPCA_SpawnCursedDrone(Player __instance)
+        {
+            Debug.Log($"[Cursed Drone] Log ");
+
+        }
+    }
+}
