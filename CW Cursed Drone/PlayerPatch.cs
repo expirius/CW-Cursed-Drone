7761f1536d7f80d09361ebe1f32ead6329c2b84c
expirius <vladworldof@gmail.com>
2025-03-19 23:50:43 +0300

    fixed add component at all

--- CW Cursed Drone/PlayerPatch.cs
+++ CW Cursed Drone/PlayerPatch.cs
@@ -1,12 +1,5 @@
-﻿using DefaultNamespace.Artifacts;
-using HarmonyLib;
-using Photon.Pun;
-using Photon.Realtime;
-using System.Collections.Generic;
-using System.Reflection;
+﻿using HarmonyLib;
 using UnityEngine;
-using UnityEngine.Localization.Settings;
-using Object = UnityEngine.Object;
 
 namespace CW_Cursed_Drone
 {
@@ -32,35 +25,43 @@ namespace CW_Cursed_Drone
         static GameObject currentDrone;// deleete
         static void CreateDrone(Player __instance) // DELETE
         {
-
-            Debug.Log("[Cursed Drone] RPCA_SpawnCursedDrone is invoked");
-
-
             // Проверяем загружен ли префаб
             if (dronePrefab == null)
             {
-                dronePrefab = CursedDroneInfo._bundle.LoadAsset<GameObject>("AltitudeHoldQuad");
+                dronePrefab = CursedDroneInfo._bundle.LoadAsset<GameObject>("Drone");
                 Debug.Log($"[Cursed Drone] Prefab is {dronePrefab.ToString()}!");
-
-                var rb = dronePrefab.AddComponent<Rigidbody>();
-                rb.useGravity = false; // Отключаем гравитацию
-                rb.drag = 1.5f; // Добавляем сопротивление для плавности
                 dronePrefab.name = "Cursed Drone";
             }
 
             var cameraTransform = __instance.refs.cameraPos.transform;
-
-            currentDrone = Object.Instantiate(
+            
+            // Создаем экземпляр объекта
+            currentDrone = GameObject.Instantiate(
                 dronePrefab,
                 cameraTransform.position + cameraTransform.forward * 2f,
                 Quaternion.identity
             );
 
-            // Добавляем компонент для управления движением
-            var controller = currentDrone.AddComponent<CursedDroneBehaviour>();
-            controller.Initialize(__instance);
+            var dronePhysics = currentDrone.AddComponent<DronePhysicsBehaviour>();
+            
+            // Находим дочерний объект "Model"
+            Transform modelTransform = currentDrone.transform.Find("Model");
+            GameObject model = modelTransform.gameObject;
 
-            Debug.Log($"[Cursed Drone] droneObj = {controller.gameObject.name} ");
+            // Добавляем AudioSource к "Model"
+            AudioSource audioSource = model.AddComponent<AudioSource>();
+            audioSource.clip = CursedDroneInfo._bundle.LoadAsset<AudioClip>("MotorSound");
+
+            // Добавляем DroneAudioBehaviour к "Model"
+            DroneSoundBehaviour audioBehaviour = model.AddComponent<DroneSoundBehaviour>();
+            audioBehaviour.motorSound = CursedDroneInfo._bundle.LoadAsset<AudioClip>("MotorSound");
+            audioBehaviour.source = audioSource;
+            audioBehaviour.dronePhysics = dronePhysics;
+            //
+            var drone = currentDrone.AddComponent<CursedDroneBehaviour>();
+            drone.Initialize(__instance);
+
+            Debug.Log($"[Cursed Drone] droneObj = {dronePrefab.gameObject.name} ");
         }
     }
 }
